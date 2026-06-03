0e0bacc28383504c621eddb1e47b6026452655c9
2005lohammy <2005lohammy@gmail,com>
2026-06-03 19:45:49 -0400

    crear list y edit

---
+++ app/src/main/java/com/example/lohammy_vasquez_ap2_p1/presentation/edit/EditRegistroScreen.kt
@@ -0,0 +1,14 @@
+package com.example.lohammy_vasquez_ap2_p1.presentation.edit
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class EditRegistroScreen {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EditBorramePreview() {
+    EditRegistroScreen(
+    )
+}
---
+++ app/src/main/java/com/example/lohammy_vasquez_ap2_p1/presentation/list/ListRegistroScreen.kt
@@ -0,0 +1,14 @@
+package com.example.lohammy_vasquez_ap2_p1.presentation.list
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class ListRegistroScreen {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ListBorramePreview() {
+    ListRegistroScreen(
+    )
+}
